5b2f51aeb244293dfb2bbfb18feb24110824596b
Alex Martinig <alexmartinig90@gmail.com>
2023-03-31 19:24:42 +0100

    added error boundry incase of issues occur

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true })
+  }
+  
+  render() {
+    if (this.state.hasError) {
+      return <h2>Sorry there appears to be a problem.</h2>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
