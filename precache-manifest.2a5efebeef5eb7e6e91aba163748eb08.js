30abe8b5de4fa9d1725868dc92937cc45ab63b6b
Alex Martinig <alexmartinig90@gmail.com>
2023-03-31 19:36:42 +0100

    Updates

---
+++ precache-manifest.2a5efebeef5eb7e6e91aba163748eb08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd965eb775429702ec9a",
+    "url": "/robofriends/static/css/main.52e40547.chunk.css"
+  },
+  {
+    "revision": "fd965eb775429702ec9a",
+    "url": "/robofriends/static/js/main.4edaaf0c.chunk.js"
+  },
+  {
+    "revision": "cb45653fdb1cd181cc7c",
+    "url": "/robofriends/static/js/runtime~main.465d8a73.js"
+  },
+  {
+    "revision": "9ef85d4636aab591b3c5",
+    "url": "/robofriends/static/css/2.a25d4d02.chunk.css"
+  },
+  {
+    "revision": "9ef85d4636aab591b3c5",
+    "url": "/robofriends/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "42e767fe25f0f677b31f3a5c8ca68132",
+    "url": "/robofriends/static/media/pixtech.42e767fe.woff"
+  },
+  {
+    "revision": "cc3de0b36f1cb9e53466ccc25d878f8d",
+    "url": "/robofriends/index.html"
+  }
+];
